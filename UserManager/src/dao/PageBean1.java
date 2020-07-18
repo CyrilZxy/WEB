28a4cf93e2d62f5497c186c5069a02094c515f8e
CyrilZxy <54348995+CyrilZxy@users.noreply.github.com>
2020-07-18 23:30:05 +0800

    Create PageBean1.java

---
+++ UserManager/src/dao/PageBean1.java
@@ -0,0 +1,14 @@
+package dao;
+
+/**
+ * @author ：ZXY
+ * @date ：Created in 2020/7/15 18:53
+ * @description：
+ */
+
+public class PageBean<T> {
+    private int totalCount;
+    private int toalPage;
+    private int x;
+
+}
